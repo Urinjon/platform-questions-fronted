b6a668392f53984a16ebb40ffe7ec35b493120cc
Freyzan2006 <freyzan2006@gmail.com>
2026-03-23 16:12:29 +0500

    fix: form profile edit

--- features/profile/model/use-profile-form.ts
+++ features/profile/model/use-profile-form.ts
@@ -9,45 +9,47 @@ import { useProfileUpdateAdapter } from "../api/use-profile.adapter";
 import type { User } from "@entities/user";
 
 export const useProfileForm = (user: User | null) => {
-	const { updateProfile, isUpdating } = useProfileUpdateAdapter();
+  const { updateProfile, isUpdating } = useProfileUpdateAdapter();
 
-	const form = useForm<ProfileFormValues>({
-		resolver: zodResolver(profileFormSchema),
-		mode: "onChange",
-		defaultValues: {
-			username: user?.username ?? "",
-			email: user?.email ?? "",
-			first_name: user?.first_name ?? "",
-			last_name: user?.last_name ?? "",
-			university: user?.university ?? "",
-			birthday: user?.birthday ? new Date(user.birthday) : undefined,
-		},
-	});
+  console.log(user?.birthday);
 
-	useEffect(() => {
-		if (user) {
-			form.reset({
-				username: user.username ?? "",
-				email: user.email ?? "",
-				first_name: user.first_name ?? "",
-				last_name: user.last_name ?? "",
-				university: user.university ?? "",
-				birthday: user.birthday ? new Date(user.birthday) : undefined,
-			});
-		}
-	}, [user, form]);
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    mode: "onChange",
+    defaultValues: {
+      username: user?.username ?? "",
+      email: user?.email ?? "",
+      first_name: user?.first_name ?? "",
+      last_name: user?.last_name ?? "",
+      university: user?.university ?? "",
+      birthday: user?.birthday ? new Date(user.birthday) : undefined,
+    },
+  });
 
-	const onSubmit = form.handleSubmit(async (data) => {
-		await updateProfile({
-			...data,
-			birthday: data.birthday
-				? data.birthday.toISOString().slice(0, 10)
-				: undefined,
-		});
-	});
+  useEffect(() => {
+    if (user) {
+      form.reset({
+        username: user.username ?? "",
+        email: user.email ?? "",
+        first_name: user.first_name ?? "",
+        last_name: user.last_name ?? "",
+        university: user.university ?? "",
+        birthday: user.birthday ? new Date(user.birthday) : undefined,
+      });
+    }
+  }, [user]);
 
-	const isDirty = form.formState.isDirty;
-	const isValid = form.formState.isValid;
+  const onSubmit = form.handleSubmit(async (data) => {
+    await updateProfile({
+      ...data,
+      birthday: data.birthday
+        ? data.birthday.toISOString().slice(0, 10)
+        : undefined,
+    });
+  });
 
-	return { form, onSubmit, isUpdating, isDirty, isValid };
+  const isDirty = form.formState.isDirty;
+  const isValid = form.formState.isValid;
+
+  return { form, onSubmit, isUpdating, isDirty, isValid };
 };
